a436fc5ef775962a5e368becca90bfa1b565be5e
Robin Vuyyuru <robinvuy@MASTUDENT-Robin-Vuyyuru-C17GMB1VQ6L4.local>
2023-05-24 11:45:47 +0100

    table format starting to implement

--- bank.js
+++ bank.js
@@ -1,20 +1,38 @@
+let table = require("table");
+let data, config;
+
 class Bank {
   constructor() { 
   this.money = 0;
+  this.date = [];
   }
 
 
   display() {
-    return this.money;
+    // return [this.date , this.money];
+
+    data = [
+      ["date", "credit", "debit", "balance"],
+      [this.date, this.date.amount, this.date.amount, this.money]
+    ]
+
+    config = {
+      border: table.getBorderCharacters("ramac"),
+    }
+
+    let x = table.table(data, config);
+    console.log(x);
+    
   }
 
-  deposit(amount) {
-    return this.money += amount;
+  deposit(amount, date) {
+    this.money += amount;
+    this.date.push(date);
     // doooo testing first numbskull
   }
 
   withdraw(amount) {
-    return this.money -= amount;
+    this.money -= amount;
   }
 
 }
--- bank.test.js
+++ bank.test.js
@@ -3,20 +3,37 @@ const Bank = require('./bank');
 describe ('bank', () => {
   it("return 0", () => {
   const account = new Bank;
-  expect(account.display()).toBe(0);
+  expect(account.display()).toContain(0);
   });
 
   it("returns 10", () => {
     const account = new Bank;
     account.deposit(10);
-    expect(account.display()).toBe(10);
-  })
+    expect(account.display()).toContain(10);
+  });
 
   it("returns 5", () =>{
     const account = new Bank;
     account.deposit(10);
     account.withdraw(5);
-    expect(account.display()).toBe(5);
-
+    expect(account.display()).toContain(5);
   })
+
+  it("returns 01/01/2023 as date of deposit", () => {
+    const account = new Bank;
+    account.deposit(10, "01/01/2023");
+    expect(account.display()).toContainEqual(["01/01/2023"]);
+  })
+
+  
+  
+  
+  
+  
+  
+  
+  
+  // it("returns a tabular display with ", () => {
+
+  // })
 })
